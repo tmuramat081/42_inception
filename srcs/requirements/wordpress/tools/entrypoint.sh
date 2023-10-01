b8be16dc1757a8ab4c69bb12ab6c6b91040bd2c0
tmuramat <mt15hydrangea@gmail.com>
2023-10-02 01:21:35 +0900

    ADD: wordpress構築途中

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
+
 set -e
+# while ! mariadbadmin ping -h"mariadb" --silent; do
+#     sleep 1
+#     counter=$((counter+1))
 
-while ! mysqladmin ping -h"mariadb" --silent; do
-    sleep 1
-done
+#     if ((counter % 10 == 0)); then
+#         echo "Still waiting for mariadb to respond. Waited ${counter} seconds."
+#     fi
 
-wp config create --dbname=wp_database --dbuser=wp_user --dbpass=password --dbhost=mariadb:3306 --allow-root && \
-wp db create --allow-root && \
+#     if ((counter > 120)); then
+#         echo "Error: mariadb did not respond after ${counter} seconds. Exiting."
+#         exit 1
+#     fi
+# done
+wait_until_db_start() {
+	echo "Waiting for MariaDB to start."
+	local i
+	for i in {10..0}; do
+	if [ "$i" = 0 ]; then
+		echo "Could not connect to MariaDB."
+	fi
+	if mariadb -h "${DB_HOST}" -u "${DB_USER}" -p"${DB_PASSWORD}" <<<'SELECT 1;' &> /dev/null; then
+		echo "MariaDB is started."
+		break
+	fi
+	sleep 1
+	done
+}
+
+wait_until_db_start
+
+wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --allow-root && \
+wp db create --allow-root & \
 wp core install --url=www.tmuramat.com --title=inception --admin_user=tmuramat --admin_password=password --admin_email=mt15hydrangea@gmail.com --allow-root
 
-exec "$@"
+echo "wordpress installed"
